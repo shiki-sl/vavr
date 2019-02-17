514ce1acde15cf6f0390ae6e7582aa627503a59d
shiki <sunlei214@foxmail.com>
2019-02-17 16:21:40 +0800

    1

---
+++ git/src/main/java/com/itheima/git/FristClass.java
@@ -0,0 +1,20 @@
+package com.itheima.git;
+
+/**
+ * @Author: shiki
+ * @Date: 2019/2/17 15:58
+ */
+public class FristClass{
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+    }
+    public void test(){
+        System.out.println("text");
+    }
+}
